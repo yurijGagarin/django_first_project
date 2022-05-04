05858ecaddd32ff59fe2278ecf2f44691652b754
yurijGagarin <yurazhagan@gmail.com>
2022-05-04 18:17:52 +0300

    Added css file

--- hypernews/settings.py
+++ hypernews/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = '(-q5783z2mh-cd217skey@(+_np2&j0=sl6suvo)ah2uwz@9ij'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 ALLOWED_HOSTS = []
 
 
--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("", views.index, name = 'index'),
@@ -7,3 +10,4 @@ urlpatterns = [
     path("news/", views.main_page),
     path("news/create/", views.create),
 ]
+urlpatterns += static(settings.STATIC_URL)
